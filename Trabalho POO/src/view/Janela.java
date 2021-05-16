71d41da22da7001b704202a306cd8d6b50c08701
Thomas Botelho <thomasjbotelho@gmail.com>
2021-05-15 22:28:27 -0300

    comeco da 2nda iteracao

---
+++ Trabalho POO/src/view/Janela.java
@@ -0,0 +1,44 @@
+package view;
+
+import models.JogoBlackjack;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.HashMap;
+import javax.swing.JFrame;
+import java.awt.Image;
+
+public class Janela extends JFrame {
+	//public JogoBlackjack jbl = JogoBlackjack.getInstancia();
+	//private List<Jogadores> jogs = new ArrayList<Jogador>();
+	protected HashMap<String, Image> assets = new HashMap<String, Image>();
+	private boolean inicializado;
+	private int largura = 400;
+	private int altura = 400;
+	
+	public Janela(String titulo) {
+		super(titulo);
+		carregarAssets();
+		this.display();
+	}
+	
+	public Image getAsset(String chave) {
+		return this.assets.get(chave);
+	}
+	
+	public void setDimensao(int largura, int altura) {
+		this.largura = largura;
+		this.altura = altura;
+	}
+	
+	public void carregarAssets() {};
+	
+	public void display() {
+		setSize(this.largura, this.altura);
+		addWindowListener( new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+	}
+}
